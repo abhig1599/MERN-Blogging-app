f76083d4dd8da06d082d8763fe84673a314e945c
Abhishek Guleria <120269927+abhig1599@users.noreply.github.com>
2024-04-03 08:27:11 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,10 @@ const Home = () => (
     <h1 className='head_text text-center'>
       Discover & Share
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+      <span className='orange_gradient text-center'>Welcome to Blogging</span>
     </h1>
     <p className='desc text-center'>
-      Promptopia is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+      Step into a world where words paint pictures, ideas spark conversations, and creativity knows no bounds. Get ready to unleash your thoughts and share your stories with the world.
     </p>
 
     <Feed />
