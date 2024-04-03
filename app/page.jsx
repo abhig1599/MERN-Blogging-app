b5e0ab2a2f3d090c5a7bd3f89044bb847116d2a6
Abhishek Guleria <120269927+abhig1599@users.noreply.github.com>
2024-04-03 08:28:56 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => (
     <h1 className='head_text text-center'>
       Discover & Share
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'>Welcome to Blogging</span>
+      <span className='orange_gradient text-center'>Blogging Here</span>
     </h1>
     <p className='desc text-center'>
       Step into a world where words paint pictures, ideas spark conversations, and creativity knows no bounds. Get ready to unleash your thoughts and share your stories with the world.
